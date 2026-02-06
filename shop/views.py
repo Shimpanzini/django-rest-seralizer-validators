710156982a5bddd2c3987afdfae6c59442276eea
Diyorbek <irmatovdiyorbek36@gmail.com>
2026-02-06 16:21:45 +0500

    add: all

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .seralizers import CategorySerializer, ProductSerializer
+from .models import Category, Product
+
 
 # Create your views here.
+
+class CategoryListCreateApiView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductListCreateApiView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
